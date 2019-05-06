8fa95a80537e4f0b35e319f5a1538e82200a0e38
Jose Garcia <j_garcia_17@hotmail.com>
2019-05-06 12:13:12 -0700

    Login and register pages code changed

--- login.php
+++ login.php
@@ -1,48 +1,51 @@
-<?php 
-require "header.php";
-require_once("dbhandler.php");
-if(isset($_POST['submit'])){
-	$email = trim($_POST['email']);
-	$password = trim($_POST['pwd']);
-	
-	$sql = "select * from users where user_email = '".$email."'";
-	$rs = mysqli_query($conn,$sql);
-	$numRows = mysqli_num_rows($rs);
-	
-	if($numRows  == 1){
-		$_SESSION['email'] = $email;
-		
-		$row = mysqli_fetch_assoc($rs);
-		if(password_verify($password,$row['user_password'])){
-			echo "Password verified";
-			header('location: loggedin/user.php');
-		}
-		else{
-			echo "Wrong Password";
-		}
-	}
-	else{
-		echo "No User found";
-	}
-}
-?>
-
-<div>
-<h1>Login</h1>
-
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-	<input type="text" name="email" value="" placeholder="Email">
-	<input type="password" name="pwd" value="" placeholder="Password">
-	<button type="submit" name="submit">Login</submit>
-</form>
-
-</div>
-
-<div>
-	<li><a href="registration.php">Register</a></li>
-</div>
-
-
 <?php
-	require "footer.php";
+	$msg = "";
+
+	if (isset($_POST['submit'])) {
+		$con = new mysqli('localhost', 'root', '', 'db_idonify');
+
+		$email = $con->real_escape_string($_POST['email']);
+		$pwd = $con->real_escape_string($_POST['pwd']);
+
+		$sql = $con->query("SELECT user_id, user_password FROM users WHERE user_email='$email'");
+		if ($sql->num_rows > 0) {
+		    $data = $sql->fetch_array();
+		    if (password_verify($pwd, $data['user_password'])) {
+		        $msg = "You have been logged IN!";
+				$_SESSION['email'] = $email;
+				header('location: user/user.php');
+            } else
+			    $msg = "Please check your inputs!";
+        } else
+            $msg = "Please check your inputs!";
+	}
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log In</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container" style="margin-top: 100px;">
+		<div class="row justify-content-center">
+			<div class="col-md-6 col-md-offset-3" align="center">
+				<img src="images/logo.png"><br><br>
+
+				<?php if ($msg != "") echo $msg . "<br><br>"; ?>
+				
+				<form method="post" action="login.php">
+					<input class="form-control" type="email" name="email" placeholder="Email"><br>
+					<input class="form-control" minlength="5" type="password" name="pwd" placeholder="Password"><br>
+					<button class="btn btn-primary" type="submit" name="submit">Log In</submit><br>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- register.php
+++ register.php
@@ -1,45 +1,61 @@
 <?php
-require "header.php"; 
-require_once("dbhandler.php");
-if(isset($_POST['submit'])){
-		$firstName = $_POST['fname'];
-		$lastName = $_POST['lname'];
-		$email 	= $_POST['email'];
-		$pwd1 = $_POST['pwd1'];
-		$pwd2 = $_POST['pwd2'];
+	$msg = "";
+
+	if (isset($_POST['submit'])) {
+		$con = new mysqli('localhost', 'root', '', 'db_idonify');
+
+		//$name = $con->real_escape_string($_POST['name']);
+		//$email = $con->real_escape_string($_POST['email']);
+		//$password = $con->real_escape_string($_POST['password']);
+		//$cPassword = $con->real_escape_string($_POST['cPassword']);
+		
+		$fname = $con->real_escape_string($_POST['fname']);
+		$lname = $con->real_escape_string($_POST['lname']);
+		$email = $con->real_escape_string($_POST['email']);
+		$pwd1 = $con->real_escape_string($_POST['pwd1']);
+		$pwd2 = $con->real_escape_string($_POST['pwd2']);
 		$userRole = "user";
 		
-		//$options = array("cost"=>4);
-		//$hashPassword = password_hash($pwd1,PASSWORD_BCRYPT,$options);
-		//$options = array("cost"=>11);
-		$hashpwd = password_hash($pwd1,PASSWORD_BCRYPT);
-		$sql = "insert into users (user_fname, user_lname,user_email, user_password, user_role) 
-		value('".$firstName."', '".$lastName."', '".$email."','".$hashpwd."','".$userRole."')";
-		$result = mysqli_query($conn, $sql);
-		if($result)
-		{
-			echo "Registration successfully";
+		if ($pwd1 != $pwd2)
+			$msg = "Please Check Your Passwords!";
+		else {
+			$hash = password_hash($pwd1, PASSWORD_BCRYPT);
+			$con->query("INSERT INTO users (user_fname, user_lname,user_email, user_password, user_role) 
+						VALUES ('$fname', '$lname', '$email', '$hash', '$userRole')");
+			$msg = "You have been registered!";
 		}
 	}
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container" style="margin-top: 100px;">
+		<div class="row justify-content-center">
+			<div class="col-md-6 col-md-offset-3" align="center">
+				<img src="images/logo.png"><br><br>
 
-<div>
-<h1>Registration</h1>
+				<?php if ($msg != "") echo $msg . "<br><br>"; ?>
+				
+				<h1>Registration</h1><br>
+				<form method="post" action="register.php">
+					<input class="form-control" minlength="3" type="text" name="fname" value="" placeholder="First Name"><br>
+					<input class="form-control" type="text" name="lname" value="" placeholder="Last Name"><br>
+					<input class="form-control" type="text" name="email" value="" placeholder="Email"><br>
+					<input class="form-control" minlength="5" type="password" name="pwd1" value="" placeholder="Password"><br>
+					<input class="form-control" minlength="5" type="password" name="pwd2" value="" placeholder="Confirm Password"><br>
+					<input class="btn btn-primary" name="submit" type="submit" value="Register"><br>
+				</form>
 
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-	<input type="text" name="fname" value="" placeholder="First Name">
-	<input type="text" name="lname" value="" placeholder="Last Name">
-	<input type="text" name="email" value="" placeholder="Email">
-	<input type="password" name="pwd1" value="" placeholder="Password">
-	<input type="password" name="pwd2" value="" placeholder="Confirm Password">
-	<button type="submit" name="submit">Submit</submit>
-</form>
-</div>
-
-<div>
-	<li><a href="login.php">Login</a></li>
-</div>
-
-<?php
-	require "footer.php";
-?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
