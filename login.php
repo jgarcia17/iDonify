fb8f97ca1a4b685b60fb5146d257be83cb8b3582
Jose Garcia <j_garcia_17@hotmail.com>
2019-05-06 12:27:32 -0700

    Added logo image and updated paths

--- login.php
+++ login.php
@@ -34,7 +34,7 @@
 	<div class="container" style="margin-top: 100px;">
 		<div class="row justify-content-center">
 			<div class="col-md-6 col-md-offset-3" align="center">
-				<img src="images/logo.png"><br><br>
+				<img src="assets/images/logo.png"><br><br>
 
 				<?php if ($msg != "") echo $msg . "<br><br>"; ?>
 				
