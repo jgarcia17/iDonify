50b25427df6a9b70960666ef0e31815806663cdb
Jose Garcia <j_garcia_17@hotmail.com>
2019-05-06 20:36:59 -0700

    Adding files to user folder

---
+++ user/user_header.php
@@ -0,0 +1,32 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="This is an example of a meta description. This will often show up in search results.">
+		<meta name=viewport content="width=device-width, initial-scale=1">
+		<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+			integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="../assets/css/style.css">
+		
+	</head>
+	<body>
+	
+		<header>
+				<nav class="navbar navbar-dark bg-dark">	
+						<ul>
+							<a class="navbar-brand" href="index.php">
+								<img src="../assets/images/logo.png" alt="logo">
+							</a>
+							<li><a href="User.php">Home</a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="donations.php">Donations</a></li>
+						</ul>
+						<li><a href="../logout.php">Logout</a></li>
+					</nav>
+			</div>
+		</header>
