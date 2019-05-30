84ae193f6d2f6aebc768f6818314b36e6223d995
jgarcia17 <j_garcia_17@hotmail.com>
2019-05-30 16:18:12 -0700

    Adding contant table update to db

--- db_idonify.sql
+++ db_idonify.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 21, 2019 at 04:15 AM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Generation Time: May 30, 2019 at 11:15 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,24 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_idonify`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `message` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`contact_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -75,6 +93,36 @@ INSERT INTO `donations` (`donation_id`, `donation_type`, `donation_qty`, `donati
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+CREATE TABLE IF NOT EXISTS `requests` (
+  `request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `device_type` varchar(100) NOT NULL,
+  `household_number` int(20) NOT NULL,
+  `household_income` int(20) NOT NULL,
+  `document` varchar(255) NOT NULL,
+  `request_description` varchar(255) NOT NULL,
+  `request_type` varchar(50) NOT NULL,
+  `device_serial` varchar(50) NOT NULL,
+  `request_date` timestamp NOT NULL,
+  `request_status` varchar(50) NOT NULL,
+  `requester_email` varchar(100) NOT NULL,
+  PRIMARY KEY (`request_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`request_id`, `device_type`, `household_number`, `household_income`, `document`, `request_description`, `request_type`, `device_serial`, `request_date`, `request_status`, `requester_email`) VALUES
+(4, 'Computer', 1, 5000, '', 'Yearly income entered.', 'device', '', '2019-05-25 07:00:00', 'in progress', 'test@email.com'),
+(5, 'Computer', 1, 5000, '', 'Screen broken, need repair.', 'service', '1234567890', '2019-05-30 07:00:00', 'in progress', 'joe@email.com');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -89,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email_confirmed` tinyint(4) NOT NULL,
   `token` varchar(10) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -97,7 +145,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`user_id`, `user_fname`, `user_lname`, `user_email`, `user_password`, `user_type`, `email_confirmed`, `token`) VALUES
 (27, 'user', 'test', 'test@email.com', '$2y$10$b9tHw6naY48Y8XshY5zE.eUpH/BdEhIxW7zcgpoSkRfxFaZICha0W', 'user', 1, ''),
-(28, 'user', 'two', 'use@email.com', '$2y$10$rlPIWcS2KGYRFLzrA6kfw.TfCQ/EavZiiqS3xhe9je2k68.0ACamK', 'user', 0, '!5kShY/K)Z');
+(28, 'user', 'two', 'use@email.com', '$2y$10$rlPIWcS2KGYRFLzrA6kfw.TfCQ/EavZiiqS3xhe9je2k68.0ACamK', 'user', 0, '!5kShY/K)Z'),
+(29, 'joe', 'test', 'joe@email.com', '$2y$10$82GBloeloat337jUOjTtneQW3d0Rjrj1w.9aw45DUidjCdsjCx0Fq', 'user', 1, '');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
