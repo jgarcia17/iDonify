6d841b75a3c357eebee0dfcfde4ad7e4f7ff87d0
jgarcia17 <j_garcia_17@hotmail.com>
2019-05-30 11:44:02 -0700

    Update to logout file

--- logout.php
+++ logout.php
@@ -2,7 +2,9 @@
 
 session_start(); //start a session
 
+unset($_SESSION['email']); 
 session_destroy(); //destroy session variables running on website
+
 header("Location: login"); //take user back to index page
 
 ?>
