eb573eddee23daebb5025cfab9c1ab1977c35bfa
jgarcia17 <j_garcia_17@hotmail.com>
2019-06-09 00:28:08 -0700

    Cleaning up files

--- Project-PHP/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start(); //start a session
-
-session_destroy(); //destroy session variables running on website
-header("Location: login.php"); //take user back to index page
-
-?>
--- old/admin/user/donations_menu.php
+++
@@ -1,8 +0,0 @@
-<ul class="nav nav-tabs nav-justified">
-  <li class="nav-item">
-    <a class="nav-link" href="donations">Device Donations</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="view_donations">Donation history</a>
-  </li>
-</ul>
